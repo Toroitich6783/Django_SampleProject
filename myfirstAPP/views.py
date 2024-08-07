831545c3e6e0b92cf225a8b7434021912f5b311b
Toroitich6783 <ropluke1@gmail.com>
2024-08-07 20:20:36 +0300

    fisrt commit

---
+++ myfirstAPP/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView 
+
+import geemap
+import ee
+import pandas as pd 
+import folium
+import geemap.foliumap as geemap
+
+Map=geemap.Map()
+
+
+# Create your views here.
+# Logic: Functions or classes
+
+class Facebook(TemplateView):
+
+
+
+
+
+
+
+
+    
+    template_name = "index.html"
+     
+
+
+class insta(TemplateView):
+    template_name = "index.html"
+    def get_context_data(self, **kwargs):
+        Map=folium.Map()
+        figure=folium.Figure()
+
+        
+
+        return{"insta":figure}
+    
+
+    
+
+    
+
+
+
+
+
+
+    
